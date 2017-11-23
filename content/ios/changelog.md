456feff43fe0a11653636fbb872521a7917fbfdf
Daniel Nielsen <dn@mapspeople.com>
2017-11-23 12:38:09 +0100

    Update changelogs script

--- content/ios/changelog.md
+++ content/ios/changelog.md
@@ -1,5 +1,7 @@
 ---
+
 title: Changelog
+
 ---
 Changelog for MapsIndoors for iOS. This document structure is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and the project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
--- update-changelogs.sh
+++ update-changelogs.sh
@@ -3,8 +3,17 @@ curl -L https://git.rtx.mapspeople.com/api/v3/projects/21/repository/raw_blobs/2
 curl -L https://git.rtx.mapspeople.com/api/v3/projects/3/repository/raw_blobs/506f11b0c970c9949daa71ea8cb8dc5d8aa92e8a?private_token=$TOKEN -o content/ios/changelog.md
 curl -L https://git.rtx.mapspeople.com/api/v3/projects/10/repository/raw_blobs/d7aa46ddc0d15487389767692e8727d9d249c656?private_token=$TOKEN -o content/cms/changelog.md
 
-REPLACEMENT="---title: Changelog---"
+REPLACE_STR="# Changelog"
+REPLACEMENT="---\n\ntitle: Changelog\n\n---"
 
-echo $REPLACEMENT|cat - content/android/changelog.md > /tmp/out && mv /tmp/out content/android/changelog.md
-echo $REPLACEMENT|cat - content/ios/changelog.md > /tmp/out && mv /tmp/out content/ios/changelog.md
-echo $REPLACEMENT|cat - content/cms/changelog.md > /tmp/out && mv /tmp/out content/cms/changelog.md
+ANDROID=`cat content/android/changelog.md`
+IOS=`cat content/ios/changelog.md`
+CMS=`cat content/cms/changelog.md`
+
+ANDROID="${ANDROID/#$REPLACE_STR/$REPLACEMENT}"
+IOS="${IOS/#$REPLACE_STR/$REPLACEMENT}"
+CMS="${CMS/#$REPLACE_STR/$REPLACEMENT}"
+
+echo -e "$ANDROID" > /tmp/out && mv /tmp/out content/android/changelog.md
+echo -e "$IOS" > /tmp/out && mv /tmp/out content/ios/changelog.md
+echo -e "$CMS" > /tmp/out && mv /tmp/out content/cms/changelog.md
